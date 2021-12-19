2edbe3746b976dae94c3f4f5943e213f74a27678
YoungJinGo <skyyj32@naver.com>
2021-12-19 20:55:15 +0900

    wine

---
+++ src/pages/wine.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next"; 
+// import가 뭔가요? 몰라도됨
+
+
+// 함수형 컴포넌트를 만들것
+// : NextPage <- 타입스크립트의 타입선언
+const WindePage: NextPage = () => {
+    return (
+        <div>
+            <h1>Wine</h1>
+        </div>
+    )
+}
+
+export default WindePage;
+// export 가 뭔가? 몰라도된다.
+//  가장 TypeScript를 쓰는 기본적인 템플릿
