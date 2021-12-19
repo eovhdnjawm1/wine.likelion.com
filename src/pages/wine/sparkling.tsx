eb0d6f95889ff3b8b55492d71e2a5e6db96a034b
YoungJinGo <skyyj32@naver.com>
2021-12-20 02:15:16 +0900

    origin

---
+++ src/pages/wine/sparkling.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next"; 
+
+const WindePage: NextPage = () => {
+    return (
+        <div>
+            <h1>sparkling Wine</h1>
+        </div>
+    )
+}
+
+export default WindePage;
---
+++ src/pages/wine/whites.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next"; 
+
+const WindePage: NextPage = () => {
+    return (
+        <div>
+            <h1>white Wine</h1>
+        </div>
+    )
+}
+
+export default WindePage;
